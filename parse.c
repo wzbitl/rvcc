c232af9daae62b877c13e797a619f5c731ca602e
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-09-10 09:37:50 +0800

    [88] 增加不完整结构体的概念

--- parse.c
+++ parse.c
@@ -1263,23 +1263,39 @@ static Type *structUnionDecl(Token **Rest, Token *Tok) {
     Tok = Tok->Next;
   }
 
+  // 构造不完整结构体
   if (Tag && !equal(Tok, "{")) {
-    Type *Ty = findTag(Tag);
-    if (!Ty)
-      errorTok(Tag, "unknown struct type");
     *Rest = Tok;
+
+    Type *Ty = findTag(Tag);
+    if (Ty)
+      return Ty;
+
+    Ty = structType();
+    Ty->Size = -1;
+    pushTagScope(Tag, Ty);
     return Ty;
   }
 
+  // ("{" structMembers)?
+  Tok = skip(Tok, "{");
+
   // 构造一个结构体
-  Type *Ty = calloc(1, sizeof(Type));
-  Ty->Kind = TY_STRUCT;
-  structMembers(Rest, Tok->Next, Ty);
+  Type *Ty = structType();
+  structMembers(Rest, Tok, Ty);
   Ty->Align = 1;
 
-  // 如果有名称就注册结构体类型
-  if (Tag)
+  // 如果是重复定义，就覆盖之前的定义。否则有名称就注册结构体类型
+  if (Tag) {
+    for (TagScope *S = Scp->Tags; S; S = S->Next) {
+      if (equal(Tag, S->Name)) {
+        *S->Ty = *Ty;
+        return S->Ty;
+      }
+    }
+
     pushTagScope(Tag, Ty);
+  }
   return Ty;
 }
 
@@ -1287,6 +1303,10 @@ static Type *structUnionDecl(Token **Rest, Token *Tok) {
 static Type *structDecl(Token **Rest, Token *Tok) {
   Type *Ty = structUnionDecl(Rest, Tok);
   Ty->Kind = TY_STRUCT;
+
+  // 不完整结构体
+  if (Ty->Size < 0)
+    return Ty;
 
   // 计算结构体内成员的偏移量
   int Offset = 0;
