490f592eb52f2b5094de2f30cf048e5d7c6bc771
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-11-04 10:50:43 +0800

    [273] 为VLA支持指针算术运算

--- codegen.c
+++ codegen.c
@@ -81,6 +81,15 @@ static void genAddr(Node *Nd) {
   switch (Nd->Kind) {
   // 变量
   case ND_VAR:
+    // Variable-length array, which is always local.
+    if (Nd->Var->Ty->Kind == TY_VLA) {
+      // printLn("  mov %d(%%rbp), %%rax", Nd->Var->Offset);
+      printLn("  li t0, %d", Nd->Var->Offset);
+      printLn("  add t0, t0, fp");
+      printLn("  ld a0, 0(t0)");
+      return;
+    }
+
     // Thread-local variable
     if (Nd->Var->IsTLS) {
       printLn("  lui a0,%%tprel_hi(%s)", Nd->Var->Name);
@@ -125,6 +134,11 @@ static void genAddr(Node *Nd) {
       return;
     }
     break;
+  case ND_VLA_PTR:
+    // printLn("  lea %d(%%rbp), %%rax", Nd->Var->Offset);
+    printLn("  li t0, %d", Nd->Var->Offset);
+    printLn("  add a0, t0, fp");
+    return;
   default:
     break;
   }
@@ -139,6 +153,7 @@ static void load(Type *Ty) {
   case TY_STRUCT:
   case TY_UNION:
   case TY_FUNC:
+  case TY_VLA:
     return;
   case TY_FLOAT:
     printLn("  # 访问a0中存放的地址，取得的值存入fa0");
