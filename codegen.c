61355540b55c7c64c0f66390336da92412919719
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-12-18 14:12:37 +0800

    [283] [GNU] 支持标签作为值

--- codegen.c
+++ codegen.c
@@ -1531,6 +1531,10 @@ static void genExpr(Node *Nd) {
 
     return;
   }
+  case ND_LABEL_VAL:
+    // printLn("  lea %s(%%rip), %%rax", Nd->UniqueLabel);
+    printLn("  la a0, %s", Nd->UniqueLabel);
+    return;
   default:
     break;
   }
@@ -1912,6 +1916,11 @@ static void genStmt(Node *Nd) {
   // goto语句
   case ND_GOTO:
     printLn("  j %s", Nd->UniqueLabel);
+    return;
+  case ND_GOTO_EXPR:
+    genExpr(Nd->LHS);
+    // println("  jmp *%%rax");
+    printLn("  jr a0");
     return;
   // 标签语句
   case ND_LABEL:
