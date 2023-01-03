f0440dadb1be872e83786ddab8bbf143d2026930
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2023-01-03 23:59:45 +0800

    [218] 数组超过16字节时，对齐值至少为16字节

--- codegen.c
+++ codegen.c
@@ -1694,10 +1694,15 @@ static void assignLVarOffsets(Obj *Prog) {
       if (Var->Offset && !Var->IsHalfByStack)
         continue;
 
+      // 数组超过16字节时，对齐值至少为16字节
+      int Align = (Var->Ty->Kind == TY_ARRAY && Var->Ty->Size >= 16)
+                      ? MAX(16, Var->Align)
+                      : Var->Align;
+
       // 每个变量分配空间
       Offset += Var->Ty->Size;
       // 对齐变量
-      Offset = alignTo(Offset, Var->Align);
+      Offset = alignTo(Offset, Align);
       // 为每个变量赋一个偏移量，或者说是栈中地址
       Var->Offset = -Offset;
       printLn(" #  寄存器传递变量%s偏移量%d", Var->Name, Var->Offset);
@@ -1735,7 +1740,11 @@ static void emitData(Obj *Prog) {
     }
 
     printLn("  # 对齐全局变量");
-    if (!Var->Ty->Align)
+    // 数组超过16字节时，对齐值至少为16字节
+    int Align = (Var->Ty->Kind == TY_ARRAY && Var->Ty->Size >= 16)
+                    ? MAX(16, Var->Align)
+                    : Var->Align;
+    if (!Align)
       error("Align can not be 0!");
     printLn("  .align %d", simpleLog2(Var->Align));
     // 判断是否有初始值
