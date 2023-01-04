f3bc2cf282b0902fe990731fc7c382adfe0202dd
wzbitl <wenzheng.bi@intel.com>
2023-01-04 17:34:38 +0800

    [1]

--- test.sh
+++ test.sh
@@ -11,12 +11,12 @@ assert() {
   # 如果运行不成功，则会执行exit退出。成功时会短路exit操作
   ./rvcc "$input" > tmp.s || exit
   # 编译rvcc产生的汇编文件
-  gcc -o tmp tmp.s
-  # $RISCV/bin/riscv64-unknown-linux-gnu-gcc -static -o tmp tmp.s
+  # gcc -o tmp tmp.s
+  $RISCV/bin/riscv64-unknown-linux-gnu-gcc -static -o tmp tmp.s
 
   # 运行生成出来目标文件
-  ./tmp
-  # $RISCV/bin/qemu-riscv64 -L $RISCV/sysroot ./tmp
+  # ./tmp
+  $RISCV/bin/qemu-riscv64 -L $RISCV/sysroot ./tmp
   # $RISCV/bin/spike --isa=rv64gc $RISCV/riscv64-unknown-linux-gnu/bin/pk ./tmp
 
   # 获取程序返回值，存入 实际值
